c957aeb9b948b8d4b0ef6a2dfdb6023dc8b77be4
Justin Michael Ramirez <JustinMichaelRamirez@gmail.com>
2018-11-22 22:23:36 -0500

    commenting out modal features until i get it right

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,15 @@
-const modal = document.getElementById('myModal');
-
-function modalShow () {
-    modal.style.display = "block";
-}
-
-function closeSpan() {
-    modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+// const modal = document.getElementById('myModal');
+//
+// function modalShow () {
+//     modal.style.display = "block";
+// }
+//
+// function closeSpan() {
+//     modal.style.display = "none";
+// }
+//
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
