c2a304bce192953046ce322611a7d6267e513fb7
Justin Michael Ramirez <JustinMichaelRamirez@gmail.com>
2020-04-24 01:56:18 -0400

    setting up branch for CRA style portfolio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Justin's CRA Portfolio</h1>
+        <p>Changes coming soon <span role="img" aria-label="sunglass emoji">😎</span></p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
